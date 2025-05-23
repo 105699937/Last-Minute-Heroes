8760ff8340238be805f443979ad7f6769474c6e1
Tanvir Rahman Tonoy <105699937@student.swin.edu.au>
2025-05-24 06:10:14 +1000

    added manage.php

---
+++ project2/manage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include './header.inc'?>
+    <title>Manage</title>
+</head>
+<body>
+    <?php include "./nav.inc" ?>
+    <main class="main_page_layout top_margin_PC top_margin_mobile">
+        <h2>
+        // we need to create the interface to manage the applications 
+        </h2>
+    </main>
+    <?php include "./footer.inc"?>
+</body>
+</html>
