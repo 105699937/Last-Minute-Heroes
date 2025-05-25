5f3f0bcee4775bac8b041a91062d0c79e1a0cea8
Tanvir Rahman Tonoy <105699937@student.swin.edu.au>
2025-05-25 22:39:13 +1000

    Moved bdImage to its corresponding folder as described in the instruction file

---
+++ project2/hr_manager_tools.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./header.inc"?>
+    <title>HR Manager Tools</title>
+</head>
+<body>
+    <?php include "./nav.inc" ?>
+    <main class="application_page_layout top_margin_PC top_margin_mobile">
+    <article>
+        <div>
+            <form action="?" method="get">
+            <input type="submit" value="list_all_eois" >
+            <input type="submit" value="FType">
+            <input type="submit" value="Filter">
+            <input type="submit" value="list_all_eois">
+            </form>
+        <div>
+        
+    </article>
+    </main>
+    <?php include "./footer.inc" ?>
+</body>
+</html>
