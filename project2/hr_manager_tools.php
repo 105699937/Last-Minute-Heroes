9d9d4b5a4c7a9d230b4bdd66824cb38961fa831f
Tanvir Rahman Tonoy <105699937@student.swin.edu.au>
2025-05-25 22:57:18 +1000

    Hr_manager_tools

--- project2/hr_manager_tools.php
+++ project2/hr_manager_tools.php
@@ -8,12 +8,13 @@
     <?php include "./nav.inc" ?>
     <main class="application_page_layout top_margin_PC top_margin_mobile">
     <article>
-        <div>
+        <div class="manager_tool_box">
             <form action="?" method="get">
             <input type="submit" value="list_all_eois" >
-            <input type="submit" value="FType">
-            <input type="submit" value="Filter">
-            <input type="submit" value="list_all_eois">
+            <input type="submit" value="Filter_by_JRN">
+            <input type="submit" value="Filter_by_Names">
+            <input type="submit" value="Delete_">
+            <input type="submit" value="Change_status">
             </form>
         <div>
         
--- project2/manage.php
+++ project2/manage.php
@@ -19,6 +19,7 @@
                 <button type="reset">Reset</button>
             </form>
         </section>
+        <code>now we need to redirect to a page where all the managers tools are available if the credintials are valid else print a message to give genuine informations and block the page after 3 wrong attempts</code>
         </article>
     </main>
     <?php include "./footer.inc" ?>
