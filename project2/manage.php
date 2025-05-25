33cf4f001144f74dccd68fdd93643217e50d6525
Tanvir Rahman Tonoy <105699937@student.swin.edu.au>
2025-05-25 19:13:06 +1000

    HR manager Page - initial design

---
+++ project2/manage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./header.inc"?>
+    <title>HR manager</title>
+</head>
+<body>
+    <?php include "./nav.inc" ?>
+    <main class="main_page_layout top_margin_PC top_margin_mobile">
+    
+    <?php echo " <h2>We need a login form so that not everyone gets the access of this page ... </h2> <p>Easy I can do it :) </p>" ?>
+    </main>
+    <?php include "./footer.inc" ?>
+</body>
+</html>
