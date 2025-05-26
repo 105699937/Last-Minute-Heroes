2ab8b1461a28ae8457ef7282b08c9497d92a9375
Tanvir Rahman Tonoy <105699937@student.swin.edu.au>
2025-05-26 21:55:32 +1000

    fixing errors

--- project2/hr_manager_tools_all_applications.php
+++ project2/hr_manager_tools_all_applications.php
@@ -31,16 +31,12 @@ if (mysqli_num_rows($result) > 0) {
             <th>Job Ref</th>
             <th>First Name</th>
             <th>Last Name</th>
-            <th> Date of Birth </th>
             <th>Street</th>
             <th>Suburb</th>
             <th>State</th>
             <th>Postcode</th>
             <th>Email</th>
             <th>Phone</th>
-            <th>Skill1</th>
-            <th>Skill2</th>
-            <th>Skill3</th>
             <th>Other Skills</th>
             <th>Status</th>
           </tr>";
@@ -51,16 +47,12 @@ if (mysqli_num_rows($result) > 0) {
                 <td>{$row['job_reference']}</td>
                 <td>{$row['first_name']}</td>
                 <td>{$row['last_name']}</td>
-                <td>{$row['dob']}</td>
                 <td>{$row['street_address']}</td>
                 <td>{$row['suburb']}</td>
                 <td>{$row['state']}</td>
                 <td>{$row['postcode']}</td>
                 <td>{$row['email']}</td>
                 <td>{$row['phone']}</td>
-                <td>{$row['skill1']}</td>
-                <td>{$row['skill2']}</td>
-                <td>{$row['skill3']}</td>
                 <td>{$row['other_skills']}</td>
                 <td>{$row['status']}</td>
               </tr>";
